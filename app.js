3c03f827a85c10339a0b17fddebcff075e165f91
Aman <aman97703@gmail.com>
2021-06-01 18:45:22 +0530

    change app

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const hbs = require('hbs');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 const jsPath = path.join(__dirname, "public/js");
 const cssPath = path.join(__dirname, "public/css");
